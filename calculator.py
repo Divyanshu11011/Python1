e294e419442c47504b5ec9a80d9fc08705cf95da
Divyanshu11011 <93030810+Divyanshu11011@users.noreply.github.com>
2023-02-25 23:34:50 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,29 @@
+def add(a, b):
+    """
+    This function takes in two numbers and returns their sum.
+    """
+    return a + b
+
+def subtract(a, b):
+    """
+    This function takes in two numbers and returns their difference.
+    """
+    return a - b
+
+def multiply(a, b):
+    """
+    This function takes in two numbers and returns their product.
+    """
+    return a * b
+
+def divide(a, b):
+    """
+    This function takes in two numbers and returns their quotient.
+    """
+    return a / b
+    
+def power(a, b):
+    """
+    This function takes in two numbers and returns the first number raised to the power of the second number.
+    """
+    return a ** b
